764b9e3449c0ed15d991be6d875201d90c03aea6
unknown <zlinus.zachrison@gmail.com>
2021-04-21 17:17:55 +0200

    adding logic for admin login

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,24 @@
 var express = require("express");
 var router = express.Router();
+const User = require("../model/User");
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
+  let template = `<h3>Please enter admin password!</h3> <form action="/admin/adminpage" method="post"> <label for="admin">Enter admin password here: </label><input type="password" id="adminPw" name="adminpassword"> <input type="submit" value="submit"></form>`
+  res.send(template);
 });
+
+router.post("/adminpage", function (req, res) {
+  console.log(req.body)
+  if(req.body.adminpassword=== "admin") {
+  console.log("du är admin")
+  res.redirect("/admin/adminUserinfo")
+  }
+  
+})
+
+router.get("/adminUserinfo", function (req, res) {
+  res.send("hej")
+})
 
 module.exports = router;
